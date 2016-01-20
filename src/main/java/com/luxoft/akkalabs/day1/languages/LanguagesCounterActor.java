6609e1f0fd53434c2832bb6decf89a292d0d2100
Dmytro_Adonin <Dmytro_Adonin@epam.com>
2016-01-20 18:42:48 +0200

    Lab 3.3

---
+++ src/main/java/com/luxoft/akkalabs/day1/languages/LanguagesCounterActor.java
@@ -0,0 +1,30 @@
+package com.luxoft.akkalabs.day1.languages;
+
+import akka.actor.UntypedActor;
+import com.luxoft.akkalabs.day1.futures.FinalResult;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dmytro_adonin on 1/20/2016.
+ */
+public class LanguagesCounterActor extends UntypedActor {
+
+    private final Map<String, Integer> languages = new HashMap<>();
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof FinalResult) {
+            FinalResult result = (FinalResult) message;
+            for (Map.Entry<String, Integer> e : result.getLanguages().entrySet()) {
+                String key = e.getKey();
+                Integer counter = languages.get(key);
+                counter = (counter == null) ? 0 : e.getValue();
+                languages.put(key, counter);
+            }
+        } else if ("get".equals(message)) {
+            getSender().tell(new HashMap(languages), getSelf());
+        }
+    }
+}
