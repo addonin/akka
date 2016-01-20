98944f6fba99625b8a325c34374958b3a2499361
dimon <dmitrii.adonin@gmail.com>
2016-01-20 09:25:07 +0200

    Lab 2.8

--- src/main/java/com/luxoft/akkalabs/day1/futures/AppleVsGoogle.java
+++ src/main/java/com/luxoft/akkalabs/day1/futures/AppleVsGoogle.java
@@ -4,6 +4,9 @@ import akka.actor.ActorSystem;
 import akka.dispatch.Futures;
 import scala.concurrent.Future;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AppleVsGoogle {
 
     public static void main(String[] args) throws Exception {
@@ -19,7 +22,10 @@ public class AppleVsGoogle {
 
         Future<FinalResult> futureGoogleResult =
                 Futures.future(google, actorSystem.dispatcher()).
-                    map(new FinalResultCalculator(), actorSystem.dispatcher());
+                        map(new FinalResultCalculator(), actorSystem.dispatcher());
+
+        List<Future<FinalResult>> allResults = Arrays.asList(futureAppleResult, futureGoogleResult);
+        Future<Iterable<FinalResult>> result = Futures.sequence(allResults, actorSystem.dispatcher());
 
         //System.out.println("Apple: ");
         //appleResult.getTweets().stream().forEach(tweet -> System.out.print(tweet.getLanguage() + " "));
