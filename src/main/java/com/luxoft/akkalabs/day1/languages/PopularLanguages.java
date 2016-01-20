ce8c0d25cca9ced0ff20bace07595a576b3a3009
dimon <dmitrii.adonin@gmail.com>
2016-01-20 23:25:14 +0200

    Lab 3.4

--- src/main/java/com/luxoft/akkalabs/day1/languages/PopularLanguages.java
+++ src/main/java/com/luxoft/akkalabs/day1/languages/PopularLanguages.java
@@ -1,8 +1,43 @@
 package com.luxoft.akkalabs.day1.languages;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.dispatch.Futures;
+import akka.dispatch.OnSuccess;
+import com.luxoft.akkalabs.day1.futures.CollectTweets;
+import com.luxoft.akkalabs.day1.futures.FinalResult;
+import com.luxoft.akkalabs.day1.futures.FinalResultCalculator;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class PopularLanguages {
 
     public static void main(String[] args) throws Exception {
-        //...        
+
+        ActorSystem actorSystem = ActorSystem.create("PopularLanguages");
+        ActorRef actorRef = actorSystem.actorOf(Props.create(LanguagesCounterActor.class));
+        List<String> keywords = Arrays.asList("Google", "Apple", "Android", "iPhone", "Lady Gaga");
+
+        for (String keyword : keywords) {
+            Futures.future(new CollectTweets(10, actorSystem, keyword), actorSystem.dispatcher())
+                    .map(new FinalResultCalculator(), actorSystem.dispatcher())
+                            .onSuccess(new SendToActor(actorRef), actorSystem.dispatcher());
+        }
+    }
+
+    private static class SendToActor extends OnSuccess<FinalResult> {
+
+        private final ActorRef actor;
+
+        public SendToActor(ActorRef actor) {
+            this.actor = actor;
+        }
+
+        @Override
+        public void onSuccess(FinalResult result) throws Throwable {
+            actor.tell(result, null);
+        }
     }
 }
