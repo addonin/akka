7c2fe92f4808d34e9ae818d853d2803415731c64
dimon <dmitrii.adonin@gmail.com>
2016-01-21 08:52:08 +0200

    Lab 5.5.2

--- src/main/java/com/luxoft/akkalabs/day1/wikipedia2/actors/ConnectionsActor.java
+++ src/main/java/com/luxoft/akkalabs/day1/wikipedia2/actors/ConnectionsActor.java
@@ -4,18 +4,32 @@ import akka.actor.UntypedActor;
 import com.luxoft.akkalabs.day1.wikipedia2.web.wikitopics.Deliver;
 import com.luxoft.akkalabs.day1.wikipedia2.web.wikitopics.Register;
 import com.luxoft.akkalabs.day1.wikipedia2.web.wikitopics.Unregister;
+import com.luxoft.akkalabs.day1.wikipedia2.web.wikitopics.WikipediaListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnectionsActor extends UntypedActor {
+
+    private final Map<String, WikipediaListener> connections = new HashMap<>();
 
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof Deliver) {
-            //...
+            Deliver deliver = (Deliver) message;
+            for (WikipediaListener listener : connections.values()) {
+                listener.deliver(deliver.getPage());
+            }
         } else if (message instanceof Register) {
-            //...
+            Register register = (Register) message;
+            connections.put(register.getListener().getStreamId(), register.getListener());
         }
         if (message instanceof Unregister) {
-            //...
+            Unregister unregister = (Unregister) message;
+            WikipediaListener listener = connections.get(unregister.getId());
+            if (listener != null) {
+                connections.remove(listener.getStreamId());
+            }
         }
     }
 }
