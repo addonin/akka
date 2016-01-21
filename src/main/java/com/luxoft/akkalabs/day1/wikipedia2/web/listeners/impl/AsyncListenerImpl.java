9a0c3b063c3e91d4ffbd30054a446514de2a6420
dimon <dmitrii.adonin@gmail.com>
2016-01-21 09:33:24 +0200

    Lab 5.8.1, 5.8.2

--- src/main/java/com/luxoft/akkalabs/day1/wikipedia2/web/listeners/impl/AsyncListenerImpl.java
+++ src/main/java/com/luxoft/akkalabs/day1/wikipedia2/web/listeners/impl/AsyncListenerImpl.java
@@ -1,5 +1,8 @@
 package com.luxoft.akkalabs.day1.wikipedia2.web.listeners.impl;
 
+import akka.actor.ActorSelection;
+import com.luxoft.akkalabs.day1.wikipedia2.web.wikitopics.Unregister;
+
 import javax.servlet.AsyncEvent;
 import javax.servlet.AsyncListener;
 import java.io.IOException;
@@ -10,23 +13,39 @@ import java.io.IOException;
  */
 public class AsyncListenerImpl implements AsyncListener {
 
+    private String streamId;
+    private ActorSelection actorSelection;
+
+    public AsyncListenerImpl(String streamId, ActorSelection actorSelection) {
+        this.streamId = streamId;
+        this.actorSelection = actorSelection;
+    }
+
+
+
     @Override
     public void onComplete(AsyncEvent asyncEvent) throws IOException {
-
+        unregister(actorSelection);
     }
 
     @Override
     public void onTimeout(AsyncEvent asyncEvent) throws IOException {
-
+        unregister(actorSelection);
+        asyncEvent.getAsyncContext().complete();
     }
 
     @Override
     public void onError(AsyncEvent asyncEvent) throws IOException {
-
+        unregister(actorSelection);
     }
 
     @Override
     public void onStartAsync(AsyncEvent asyncEvent) throws IOException {
 
     }
+
+    private void unregister(ActorSelection selection) {
+        selection.tell(new Unregister(streamId), null);
+    }
+
 }
